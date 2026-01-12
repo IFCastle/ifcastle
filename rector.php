ad3ba305174d1ca6c20c0c46957bca67698ef42c
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2026-01-12 19:45:38 +0200

    Add branch-alias and update PHP requirements in composer.json

---
+++ rector.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use IfCastle\CodeStyle\Rector\RectorConfigurator;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->paths([
+        __DIR__ . '/packages',
+    ]);
+    
+    (new RectorConfigurator)->configureSets($rectorConfig);
+};
